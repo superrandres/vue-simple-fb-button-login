89834c8d2b07bb01ce1dee2ab5d9a7bce5133024
Ronald Espinoza <ing.ronaldespinoza@gmail.com>
2020-05-09 20:10:19 -0500

    funcionando con roollup https://dev.to/amroessam/publish-your-first-npm-package-vue-263h

---
+++ src/entry.js
@@ -0,0 +1,26 @@
+import component from "./SimpleFBButtonLogin.vue";
+
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("simple-fb-button-login", component);
+}
+
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+component.install = install;
+
+export default component;
